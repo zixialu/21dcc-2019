66491f2592b15dd66476a10ef8fddcf0c448cd79
Zixia Lu <zixialu@gmail.com>
2019-05-06 08:14:25 -0400

    Add challenge 5

---
+++ challenge05/solution.js
@@ -0,0 +1,14 @@
+function loadModule(index) {
+  const moduleToLoad = availableModules[index];
+  moduleToLoad.enabled = true;
+  ship.modules.push(moduleToLoad);
+}
+
+function findModuleIndex(name) {
+  return availableModules.findIndex(module => (
+    module.essential && module.name === name
+  ));
+}
+
+loadModule(findModuleIndex('life-support'));
+loadModule(findModuleIndex('propulsion'));
