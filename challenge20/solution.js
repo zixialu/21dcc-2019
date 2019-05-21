a82bb417bbb66cb1f0fb0c72157b00e731037515
Zixia Lu <zixialu@gmail.com>
2019-05-20 23:24:41 -0400

    Shorter solution for challenge 20

--- challenge20/solution.js
+++ challenge20/solution.js
@@ -1,15 +1,8 @@
 function decodeMessage(message) {
   const matchDigit = /\d/g;
   const decoder = (match) => {
-    switch (match) {
-      case '0': return 'o';
-      case '1': return 'i';
-      case '2': return 'u';
-      case '3': return 'e';
-      case '4': return 'a';
-      case '5': return 'y';
-      default: return match;
-    }
+    const vowels = ['o', 'i', 'u', 'e', 'a', 'y'];
+    return vowels[match] || match;
   };
   return message.replace(matchDigit, decoder);
 }
